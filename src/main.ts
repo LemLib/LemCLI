7a1e8920122ca88f759e5f24551cd7e1a8df5fde
meisZWFLZ <atcurtis08@gmail.com>
2023-11-24 21:37:47 -0700

    Add src

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+var argv = require('yargs/yargs')(process.argv.slice(2)).argv;
+
+if (argv.ships > 3 && argv.distance < 53.5) {
+    console.log('Plunder more riffiwobbles!');
+} else {
+    console.log('Retreat from the xupptumblers!');
+}
